737c24dcb0b87b85bbf4784f458c2ab74ad71995
Onuram <gsmaruno@gmail.com>
2025-03-13 21:55:45 -0300

    Create project
    Creating

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Com base neste Script - vamos melhorar nosso carro e deixa-lo mais tunano 😜
+//CRIAR UM CARRO COM MOTOR 4 POR 4
+//CRIAR UM CARRO COM FREIO NAS QUATRO RODAS
+//CRIAR UM JIP E UM CAMINHÃO.😱 
+//CRIE UM FREIO DE MÃO .
+//ACENDER E APAGAR A LUZ DO CARRO
+
+public class CarController : MonoBehaviour
+{    
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentBreakForce;
+    private bool isBreaking;
+
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRighttWheelCollider;
+    [SerializeField] private WheelCollider RearLeftWheelCollider;
+    [SerializeField] private WheelCollider RearRightWheelCollider;
+
+    
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRighttWheelTransform;
+    [SerializeField] private Transform RearLeftWheelTransformr;
+    [SerializeField] private Transform RearRightWheelTransform;
+
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteeringAngle;
+
+
+    [SerializeField] private Light lightR;
+    [SerializeField] private Light lightL;
+
+    private void FixedUpdate() 
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        RestartPosition();
+        ToggleHeadlights();
+        Handbreak();
+
+    }
+
+    private void RestartPosition()
+    {
+       if(Input.GetKey("r"))
+       {
+           Debug.Log("RestartPosition");
+           transform.position = new Vector3(3f, transform.position.y, transform.position.z);
+           transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+       }
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRighttWheelCollider.motorTorque = verticalInput * motorForce;
+
+        //CRIAR UM CARRO COM MOTOR 4 POR 4:
+        RearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        RearRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();   
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRighttWheelCollider.brakeTorque = currentBreakForce;
+        frontLeftWheelCollider.brakeTorque = currentBreakForce;
+
+        //CRIAR UM CARRO COM FREIO NAS QUATRO RODAS:
+        RearLeftWheelCollider.brakeTorque = currentBreakForce;
+        RearRightWheelCollider.brakeTorque = currentBreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRighttWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+       UpdateSingleWheelCollider(frontLeftWheelCollider, frontLeftWheelTransform);
+       UpdateSingleWheelCollider(frontRighttWheelCollider, frontRighttWheelTransform);
+       UpdateSingleWheelCollider(RearRightWheelCollider, RearRightWheelTransform);
+       UpdateSingleWheelCollider(RearLeftWheelCollider, RearLeftWheelTransformr);
+       
+    }
+
+    private void UpdateSingleWheelCollider( WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos,  out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+
+    //CRIE UM FREIO DE MÃO 
+    private void Handbreak()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyBreaking();
+        }
+    }
+
+    //ACENDER E APAGAR A LUZ DO CARRO
+    private void ToggleHeadlights()
+    {
+        if (Input.GetKey("l"))
+        {
+            if (lightL.intensity == 0)
+            {
+                lightL.intensity = 300;
+                lightR.intensity = 300;
+            }
+            else
+            {
+                lightL.intensity = 0;
+                lightR.intensity = 0;
+
+            }
+        }
+    }
+
+}
